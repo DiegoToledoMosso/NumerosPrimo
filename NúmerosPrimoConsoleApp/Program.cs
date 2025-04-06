47e932aca1a5db7a01621bf5ed04818d2d075ab3
Diego Toledo <Diego_toledo3010@hotmail.com>
2025-04-06 03:58:08 -0300

    Finalizendo o programa

--- NúmerosPrimoConsoleApp/Program.cs
+++ NúmerosPrimoConsoleApp/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+
+
+                int divisor = 0;
+                Console.Clear();
+                Console.WriteLine("--------------------------------------------------");
+                Console.WriteLine("Olá, aqui iremos verificar se o número digitado é primo.");
+                Console.WriteLine("--------------------------------------------------");
+
+                Console.WriteLine("Por favor digite o número para verifição:");                
+
+                int numero = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 1; i <= numero; i++)
+                {
+                    if (numero / i == 0)
+                        divisor++;
+                }
+                if (divisor == 2)
+                    Console.WriteLine("Este número é primo");
+                else
+                    Console.WriteLine("Este número não é primo");
+
+
+                Console.Write("Deseja continuar? (S/N): ");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+                if (opcaoContinuar != "S")
+                    break;
+
+            }
         }
     }
 }
